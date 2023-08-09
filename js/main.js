d3f668a4d8701439efaacd81cb35cc6ffe914a24
Samaaaaaa <sa2606@fayoum.edu.eg>
2023-08-09 12:42:11 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,203 @@
+function show1(){
+    var cartona = `<h3>الكندوز</h3>
+    <div class="row text-center">
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 1.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 2.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 3.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 4.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 5.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 6.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 7.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 8.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 9.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 10.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 11.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 12.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 13.png" alt="" />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4 g-3">
+        <div class="p-3 width">
+          <img src="./img/Group 14.png" alt="" />
+        </div>
+      </div>
+    </div>
+    `
+
+    document.getElementById("page1").innerHTML = cartona
+
+}
+
+function show2(){
+  var cartona = `<h3>جاهز للتسوية</h3>
+  <div class="row text-center">
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 1.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 2.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 3.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 4.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 5.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 6.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 7.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 8.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 9.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image2/Group 10.png" alt="" />
+      </div>
+    </div>
+  </div>
+  `
+
+  document.getElementById("page1").innerHTML = cartona
+
+}
+function show3(){
+  var cartona = `<h3>حجز مسبق</h3>
+  <div class="row text-center">
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 1.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 2.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 3.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 4.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 5.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image3/Group 6.png" alt="" />
+      </div>
+    </div>
+  </div>
+  `
+
+  document.getElementById("page1").innerHTML = cartona
+
+}
+function show4(){
+  var cartona = `<h3>الضاني</h3>
+  <div class="row text-center">
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image4/Group 1.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image4/Group 2.png" alt="" />
+      </div>
+    </div>
+    <div class="col-md-6 col-lg-4 g-3">
+      <div class="p-3 width">
+        <img src="./image4/Group 4.png" alt="" />
+      </div>
+    </div>
+  </div>
+  `
+
+  document.getElementById("page1").innerHTML = cartona
+
+}
+
